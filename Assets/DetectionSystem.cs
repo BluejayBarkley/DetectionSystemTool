c18ee1b2b35657b42940cc03ab4618dc31db9165
BluejayBarkley <ulyhargett@gmail.com>
2023-12-05 21:45:46 -0600

    Final Version of Code Version 01
    Code has been cleaned and restructured for efficiency. OnExit switch statements implemented alongside LineOfSight switch statements.

--- Assets/DetectionSystem.cs
+++ Assets/DetectionSystem.cs
@@ -6,19 +6,25 @@ using UnityEngine.Events;
 public class DetectionSystem : MonoBehaviour
 {
     [Header("Game Object to Detect")]
+    //Drag in the specific game object the detector is watching for.
     [SerializeField] private GameObject _objectToDetect;
+    //A bool statement set to false by default so the object is not detected on game start.
     [SerializeField][Tooltip("The object is not detected by default on game start.")]
     public bool objectDetected = false;
 
     [Header("Line of Sight")]
+    //A boolean used to determine if the detector requires line of sight when detecting an object. True means line of sight is required, and false means it is not.
     [SerializeField][Tooltip("Check if line of sight is required for detection.")]
     bool _lineOfSightRequired;
+    //Line of sight uses raycasting, so the raycastOrigin is the point from which the raycast originates.
     [SerializeField][Tooltip("Line of Sight point of origin.")]
     GameObject _raycastOrigin;
 
     [Header("Method of Detection")]
     [SerializeField] private DetectionMethod _detectionMethod;
+    //If using Tags to detect, input the Tag name to detect.
     [SerializeField] string _tagToDetect;
+    //If using Layers to detect, input the Layer name to detect.
     [SerializeField] LayerMask _layerToDetect;
     private enum DetectionMethod
     {
@@ -28,32 +34,119 @@ public class DetectionSystem : MonoBehaviour
     }
 
     [Header("Time Until Detector Forgets")]
+    //DetectLeaveDuration allows the detector to maintain detection on the object after the object has left detection range. Set to 0 to disable this feature.
     [SerializeField][Tooltip("The amount of time in seconds it takes for the detector to forget the object after it leaves the detection range.")] 
     public float DetectLeaveDuration = 2.0f;
     private IEnumerator coroutine;
 
     [Header("Events to be Run")]
+    //DetectedWithoutLineOfSight will be used for events that require detection without a line of sight requirement.
     public UnityEvent DetectedWithoutLineOfSight;
+    //While DetectedWithLineOfSight detects with a line of sight requirement.
     public UnityEvent DetectedWithLineOfSight;
 
     private void Awake()
     {
+        //To ensure that the DetectLeaveDuration does not cause issues on game start.
         coroutine = leftRange();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //If the object to detect enters the trigger volume of the detector...
         if (objectDetected)
         {
+            //and if line of sight is not required...
+            if(_lineOfSightRequired == false)
+            {
+                //Continously run the event where the object is detected and line of sight is not required.
+                DetectedWithoutLineOfSight.Invoke();
+            }
+
+            //Continuously run the line of sight code.
             LineOfSight();
         }
 
-        //if(MoveTowards working tests)
-        
     }
 
     private void OnTriggerEnter(Collider other)
+    {
+        switch (_detectionMethod)
+        {
+
+            //Detection method set to Tag.
+            case DetectionMethod.Tag:
+                //Comparing the tag of the detected object with the tag to detect.
+                if (other.tag == _tagToDetect)
+                {
+                    //If line of sight is not required...
+                    if (_lineOfSightRequired == false)
+                    {
+                        Debug.Log("Detected " + _tagToDetect + " Tag, LoS not needed.");
+                        //Then the object is detected.
+                        objectDetected = true;
+
+                        //Run the code to detect when the object has left the detection range.
+                        ObjectLeavingRange();
+                        
+                    }
+
+                    //If line of sight is required...
+                    if (_lineOfSightRequired == true)
+                    {
+                        Debug.Log("Detected " + _tagToDetect + " Tag, LoS needed.");
+                        objectDetected = true;
+                        ObjectLeavingRange();
+                    }
+                }
+                break;
+
+            //Detection method set to Layer.
+            case DetectionMethod.Layer:
+                //Mathf.Pow is required to compare layer values to the power of 2.
+                if (Mathf.Pow(2, other.gameObject.layer) == _layerToDetect.value)
+                {
+                    if (_lineOfSightRequired == false)
+                    {
+                        Debug.Log("Detected Player Layer, LoS not needed.");
+                        objectDetected = true;
+                        ObjectLeavingRange();
+                    }
+
+                    if (_lineOfSightRequired == true)
+                    {
+                        Debug.Log("Layer, LoS needed.");
+                        objectDetected = true;
+                        ObjectLeavingRange();
+                    }
+                }
+                break;
+
+            //Detection method set to GameObject.
+            case DetectionMethod.GameObject:
+                if (other.gameObject == _objectToDetect)
+                {
+                    if (_lineOfSightRequired == false)
+                    {
+                        Debug.Log("Detected Player GameObject, LoS not needed.");
+                        objectDetected = true;
+                        ObjectLeavingRange();
+                    }
+
+                    if (_lineOfSightRequired == true)
+                    {
+                        Debug.Log("GameObject, LoS needed.");
+                        objectDetected = true;
+                        ObjectLeavingRange();
+                    }
+                }
+                break;
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
     {
         switch (_detectionMethod)
         {
@@ -63,53 +156,32 @@ public class DetectionSystem : MonoBehaviour
                 {
                     if (_lineOfSightRequired == false)
                     {
-                        Debug.Log("Detected " + _tagToDetect + " Tag, LoS not needed.");
-                        objectDetected = true;
+                        Debug.Log("Player Exited Collider. Tag, NLOS.");
+                        StartCoroutine(coroutine);
 
-                        DetectedWithoutLineOfSight.Invoke();
-
-                        ObjectLeavingRange();
-                        
                     }
 
                     if (_lineOfSightRequired == true)
                     {
-                        Debug.Log("Detected " + _tagToDetect + " Tag, LoS needed.");
-                        //LineOfSight();
-                        objectDetected = true;
-
-                        DetectedWithLineOfSight.Invoke();
-
-                        ObjectLeavingRange();
+                        Debug.Log("Player Exited Collider. Tag, LOS.");
+                        StartCoroutine(coroutine);
                     }
                 }
                 break;
 
             case DetectionMethod.Layer:
-                Debug.Log("_layerToDetect.value: " + _layerToDetect.value);
-                Debug.Log("gameObject.layer: " + other.gameObject.layer);
-                Debug.Log("LayerToName: " + LayerMask.LayerToName(3));
-                Debug.Log("NameToLayer: " + LayerMask.NameToLayer("Player"));
-                Debug.Log("GetMask: " + LayerMask.GetMask("Player"));
-                //var _layerName = LayerMask.LayerToName(3);
-                //if (other.gameObject.layer == _layerToDetect.value)
-                //Mathf.Pow(2, other.gameObject.layer)
                 if (Mathf.Pow(2, other.gameObject.layer) == _layerToDetect.value)
                 {
                     if (_lineOfSightRequired == false)
                     {
-                        Debug.Log("Detected Player Layer, LoS not needed.");
-                        objectDetected = true;
-                        DetectedWithoutLineOfSight.Invoke();
-                        ObjectLeavingRange();
+                        Debug.Log("Player Exited Collider. Layer, NLOS.");
+                        StartCoroutine(coroutine);
                     }
 
                     if (_lineOfSightRequired == true)
                     {
-                        Debug.Log("Layer, LoS needed.");
-                        objectDetected = true;
-                        DetectedWithLineOfSight.Invoke();
-                        ObjectLeavingRange();
+                        Debug.Log("Player Exited Collider. Layer, LOS.");
+                        StartCoroutine(coroutine);
                     }
                 }
                 break;
@@ -119,34 +191,17 @@ public class DetectionSystem : MonoBehaviour
                 {
                     if (_lineOfSightRequired == false)
                     {
-                        Debug.Log("Detected Player GameObject, LoS not needed.");
-                        objectDetected = true;
-                        DetectedWithoutLineOfSight.Invoke();
-                        ObjectLeavingRange();
+                        Debug.Log("Player Exited Collider. Object, NLOS.");
+                        StartCoroutine(coroutine);
                     }
 
                     if (_lineOfSightRequired == true)
                     {
-                        Debug.Log("GameObject, LoS needed.");
-                        objectDetected = true;
-                        DetectedWithLineOfSight.Invoke();
-                        ObjectLeavingRange();
+                        Debug.Log("Player Exited Collider. Object, LOS.");
+                        StartCoroutine(coroutine);
                     }
                 }
                 break;
-
-        }
-    }
-
-
-    //ADD SWITCH STATEMENTS
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.tag == _tagToDetect || other.gameObject.layer == _layerToDetect.value || other.gameObject == _objectToDetect)
-        {
-            //Debug.Log("Player Exited Collider.");
-            StartCoroutine(coroutine);
         }
     }
 
@@ -166,23 +221,25 @@ public class DetectionSystem : MonoBehaviour
         Debug.Log("Player Not Detected");
     }
 
-    public void MoveToPlayer()
+    public void MoveToObject()
     {
-        Vector3.MoveTowards(gameObject.transform.position, _objectToDetect.transform.position, 2);
+        float speed = 1.0f;
+        var step = speed * Time.deltaTime;
+        gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, _objectToDetect.transform.position, step);
     }
 
-    public void LineOfSight()
+    public void LookAtObject()
     {
+        gameObject.transform.LookAt(_objectToDetect.transform.position);
+    }
 
-        //Debug.Log("LineOfSight invoked.");
-        
+    //Method used to raycast for line of sight detection requirements.
+    public void LineOfSight()
+    {       
         Vector3 rayStartPos = _raycastOrigin.transform.position;
-        //Vector3 rayDirection = gameObject.transform.forward;
 
         if (objectDetected)
         {
-            //Debug.Log("LineOfSight objectDetected.");
-
             Vector3 rayDirection = _objectToDetect.transform.position - _raycastOrigin.transform.position;
             Ray ray = new Ray(rayStartPos, rayDirection);
             Debug.DrawRay(ray.origin, ray.direction, Color.red);
@@ -191,13 +248,41 @@ public class DetectionSystem : MonoBehaviour
 
             if (hitData.transform != null)
             {
-                //Debug.Log("Detected Object: " + hitData.transform.gameObject.name);
 
-
-                if (hitData.transform.gameObject.tag == "Player")
+                switch (_detectionMethod)
                 {
-                    //Debug.Log("Detected Object: " + hitData.transform.gameObject.name);
-                    gameObject.transform.LookAt(_objectToDetect.transform.position);
+
+                    case DetectionMethod.Tag:
+                        if (hitData.transform.gameObject.tag == _tagToDetect)
+                        {
+                            //If line of sight is required...
+                            if (_lineOfSightRequired == true)
+                            {
+                                //Run the event where line of sight is required.
+                                DetectedWithLineOfSight.Invoke();
+                            }
+                        }
+                        break;
+
+                    case DetectionMethod.Layer:
+                        if (Mathf.Pow(2, hitData.transform.gameObject.layer) == _layerToDetect.value)
+                        {
+                            if (_lineOfSightRequired == true)
+                            {
+                                DetectedWithLineOfSight.Invoke();
+                            }
+                        }
+                        break;
+
+                    case DetectionMethod.GameObject:
+                        if (hitData.transform.gameObject == _objectToDetect)
+                        {
+                            if (_lineOfSightRequired == true)
+                            {
+                                DetectedWithLineOfSight.Invoke();
+                            }
+                        }
+                        break;
                 }
             }
         }
